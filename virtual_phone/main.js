5895a196c273b2823d6d133989b1fd36164f5449
Ingridoliveira2014 <ingridoliveirag01@gmail.com>
2025-05-22 22:21:23 -0300

    virtual phone parte 2

---
+++ virtual_phone/main.js
@@ -0,0 +1,25 @@
+const display = document.querySelector('input[type=tel]');
+
+const teclas = document.querySelectorAll('input[type=button]');
+
+const limpar = document.querySelector('.icon');
+
+limpar.onclick = () => {
+    const opcao = confirm('Deseja mesmo limpar?')
+    if(opcao) {
+        display.value = "";
+    }
+    
+}
+//tarefinha: criar uma condição para travar
+//mais de 12 digitos
+function valorTeclaToDisplay (i) {
+    display.value += teclas[i].value;
+}
+
+
+for (let i = 0; i < teclas.length; i++){
+      teclas[i].onclick = () => {
+        valorTeclaToDisplay(i);
+      }
+    }
